ee50c3e5d8180303a79d39708ed69a1209a613ad
Sudeep Mahato <jayamahato3245@gmail.com>
2023-07-22 14:40:17 -0400

    Added all the july 22 class file

---
+++ src/july22/ArraysPractice.java
@@ -0,0 +1,17 @@
+package july22;
+
+import java.util.Scanner;
+
+public class ArraysPractice {
+    public static void main(String[] args) {
+        String vegitable[] =new String[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Vegitable name:");
+        for (int i = 0; i < vegitable.length; i++) {
+            vegitable[i]=scanner.next();
+        }
+        for (int i = 0; i < vegitable.length; i++) {
+            System.out.println(vegitable[i]);
+        }
+    }
+}
---
+++ src/july22/Main.java
@@ -0,0 +1,17 @@
+package july22;
+
+public class Main {
+    public static void main(String[] args) {
+        String vagitable[] = new String[4];
+        vagitable[0] = "potato";
+        vagitable[1] = "eggplant";
+        vagitable[2] = "oakra";
+        vagitable[3] = "cabage";
+
+        System.out.println(vagitable[0]);
+        System.out.println(vagitable[1]);
+        System.out.println(vagitable[2]);
+        System.out.println(vagitable[3]);
+
+    }
+}
