bbde581803956414c959490434c4d4f2905bdcb1
Sudeep Mahato <jayamahato3245@gmail.com>
2023-07-29 14:03:45 -0400

    updated

--- src/july29/StringReverse.java
+++ src/july29/StringReverse.java
@@ -15,7 +15,7 @@ public class StringReverse {
         }
         for (int i = charArray.length-1; i >=0 ; i--) {
             System.out.println(" ");
-            System.out.println(" "+charArray);
+            System.out.println(" "+charArray[i]);
         }
 
     }
