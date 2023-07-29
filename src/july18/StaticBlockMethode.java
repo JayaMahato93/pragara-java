a71102581bfda21d93548b01f4a6ff03f02401da
Sudeep Mahato <jayamahato3245@gmail.com>
2023-07-29 09:59:47 -0400

    updated

--- src/july18/StaticBlockMethode.java
+++ src/july18/StaticBlockMethode.java
@@ -5,7 +5,8 @@ public class StaticBlockMethode {
         System.out.println("Print fist...");
     }
     public static void main(String[] args) {
-        System.out.println("Inside main mehod... ");
+
+        System.out.println("Inside main method... ");
     }
     static {
         System.out.println("Print 2...");
